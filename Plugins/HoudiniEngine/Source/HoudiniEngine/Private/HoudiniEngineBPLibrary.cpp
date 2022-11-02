08f5f5f92b68e8599bb62c03fbd261ef51585752
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2022-11-02 17:46:15 +0800

    Add files via upload

---
+++ Plugins/HoudiniEngine/Source/HoudiniEngine/Private/HoudiniEngineBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HoudiniEngineBPLibrary.h"
+#include "HoudiniEngine.h"
+
+UHoudiniEngineBPLibrary::UHoudiniEngineBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UHoudiniEngineBPLibrary::HoudiniEngineSampleFunction(float Param)
+{
+	return -1;
+}
+
