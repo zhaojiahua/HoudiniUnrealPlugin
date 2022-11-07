567e4e95a706c77f7fec876ccaeb882efd20e817
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2022-11-07 18:17:43 +0800

    Delete Source directory

--- Source/HoudiniUnrealPlugin/HoudiniUnrealPlugin.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HoudiniUnrealPlugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HoudiniUnrealPlugin, "HoudiniUnrealPlugin" );
- 
